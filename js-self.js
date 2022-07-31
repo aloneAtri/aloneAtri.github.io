de89549c8ec8885dac5718bfb74fd581d7b4332a
aloneAtri <108735570+aloneAtri@users.noreply.github.com>
2022-08-01 01:24:51 +0800

    111

--- js-self.js
+++ js-self.js
@@ -1,6 +1,24 @@
 // var img = document.getElementById('img01');
 var body = document.body;//获取body元素
 var img = document.querySelector('.background').querySelectorAll('img');//获取背景图片
+var nav = document.querySelector('.nav').querySelectorAll('a');//获取导航栏
+var nav1 = document.querySelector('#nav1');//下拉列表
+var theme = document.querySelectorAll('.image01');
+
+// 导航栏下拉列表
+nav[2].onmousemove = function () {
+    nav1.style.display = 'block';
+}
+nav[2].onmouseout = function () {
+    nav1.style.display = 'none';
+}
+nav1.onmousemove = function () {
+    nav1.style.display = 'block';
+}
+nav1.onmouseout = function () {
+    nav1.style.display = 'none';
+}
+
 
 var mouseImg = document.querySelector('#mouse-move')//获取鼠标移动元素并创建移动事件
 // 鼠标移动特效函数
@@ -15,8 +33,8 @@ document.addEventListener('mousemove', function (e) {
 //图片切换函数
 time1();
 function time1() {
-    // body.style.backgroundImage = 'url(' + './background/背景' + getRandom(1, 12) + '.png)';
-    body.style.backgroundImage = 'url(' + img[getRandom(1, 13)].src + ')';
+    body.style.backgroundImage = 'url(' + './background/背景' + getRandom(1, 13) + '.png)';
+    // body.style.backgroundImage = 'url(' + img[getRandom(1, 13)].src + ')';
 }
 setInterval(time1, 5000)
 
